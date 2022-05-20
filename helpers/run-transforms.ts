1efd7d3a4a47fdb09684a9a32c45f1f59842e837
Romain Gilliotte <rgilliotte@gmail.com>
2022-05-20 18:18:27 +0200

    refactor: useless types

--- helpers/run-transforms.ts
+++ helpers/run-transforms.ts
@@ -3,7 +3,7 @@ import { API, FileInfo } from "jscodeshift";
 type Transform = (file: FileInfo, api: API) => string;
 
 export default function (transforms: Transform[]): Transform {
-  return function (fileInfo: FileInfo, api: API): string {
+  return function (fileInfo, api) {
     let source = fileInfo.source;
 
     for (const fix of transforms) {
