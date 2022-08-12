8e76bccf30bccc8f3c4d66f23bffe5b0ec741547
Arnaud Moncel <arnaud.moncel43@gmail.com>
2022-08-12 09:57:23 +0200

    feat: v7v8 is now ready to use

--- src/commands/V7-V8.ts
+++ src/commands/V7-V8.ts
@@ -5,13 +5,33 @@ import { run } from 'jscodeshift/src/Runner';
 
 export default class V7toV8 extends Command {
   static override description = 'Migrate your v7 agent to V8';
+  static excludedFolderToExtract = ['node_modules'];
+
+  extractFiles(folderPath: string): String[] {
+    const dirents = fs.readdirSync(folderPath, { withFileTypes: true });
+
+    return dirents.flatMap(dirent => {
+      if (V7toV8.excludedFolderToExtract.includes(dirent.name)) return null;
+
+      const direntPath = path.join(folderPath, dirent.name);
+
+      if (dirent.isDirectory()) return this.extractFiles(direntPath);
+      if (path.extname(dirent.name) === '.js') return direntPath;
+    }).filter(Boolean);
+  }
 
   async run(): Promise<any> {
+    const projectPath = process.cwd();
+    const filesPath = this.extractFiles(projectPath);
+
+    const transformPath = `../mods/v7-v8/index.${process.env.NODE_ENV === 'development' ? 'ts' : 'js'}`;
+
     const res = await run(
-      path.join(__dirname, '../mods/v7-v8/index.ts'),
-      [path.join(__dirname, '../../files-to-mod')],
-      { babel: true, parser: 'flow' },
+      path.join(__dirname, transformPath),
+      filesPath,
+      { babel: true, parser: 'flow', verbose: 2 },
     );
+
     this.log(res);
   }
 }
